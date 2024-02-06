209a6ad2b7928db173e60cea3a1d139546e50fc3
cammartinezx <camila.martinez2002@gmail.com>
2024-02-05 23:16:12 -0600

    added ui for login

--- app/src/main/java/com/well_sync/presentation/LoginActivity.java
+++ app/src/main/java/com/well_sync/presentation/LoginActivity.java
@@ -1,4 +1,28 @@
 package com.well_sync.presentation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.well_sync.R;
 
-public class LoginActivity {
-}
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView signUpButton = findViewById(R.id.SignUp);
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
+
+    }
+
+
+
+    }
