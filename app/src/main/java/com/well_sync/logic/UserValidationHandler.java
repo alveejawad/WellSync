344aea1ee7470548a64f400c1c462bb5add4a697
Gabriel Young <youngg2@myumanitoba.ca>
2024-02-06 12:41:37 -0600

    Write user validation handler; update Patient DSO

--- app/src/main/java/com/well_sync/logic/UserValidationHandler.java
+++ app/src/main/java/com/well_sync/logic/UserValidationHandler.java
@@ -5,20 +5,18 @@ import com.well_sync.objects.UserCredentials;
 
 public class UserValidationHandler {
 
-	// TODO: this doesn't exist yet
-	//  private UserRegistrationPersistence persistUsers;
+    private UserPersistence persistUsers;
 
-	public UserValidationHandler() {
-		// TODO
-	}
+    public UserValidationHandler() {
+        // TODO
+    }
 
-	public boolean login(UserCredentials credentials) {
-		// TODO
-		return false;
-	}
-	
-	public boolean register(UserCredentials credentials, Patient details) {
-		// TODO
-		return false;
-	}
+    public boolean login(UserCredentials inputCreds) {
+        UserCredentials savedCreds = persistUsers.getUserCredentials(inputCreds);
+        return inputCreds.equals(savedCreds);
+    }
+
+    public void register(UserCredentials credentials) {
+        persistUsers.setUserCredentials(credentials);
+    }
 }
