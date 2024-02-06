05a82d98fd076157be412e330281fcf8fc780bf4
Gabriel Young <youngg2@myumanitoba.ca>
2024-02-06 11:43:11 -0600

    Add new DSOs to handler stubs

--- app/src/main/java/com/well_sync/logic/PatientHandler.java
+++ app/src/main/java/com/well_sync/logic/PatientHandler.java
@@ -1,6 +1,19 @@
 package com.well_sync.logic;
 
+import com.well_sync.objects.Patient;
+import com.well_sync.objects.UserCredentials;
+
 public class PatientHandler {
+
+	public Patient getDetails(UserCredentials credentials) {
+		// TODO
+		return null;
+	}
+	
+	public void editDetails(UserCredentials credentials, Patient details) {
+		// TODO
+		return;
+	}
 }
 
 
