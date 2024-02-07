c20011990383c6987fbda79899e2f7b0246faaa4
Ryan Austria Velicaria <velicar1@myumanitoba.ca>
2024-02-07 00:19:10 -0600

    Changes to be committed: modified: MoodLogHandler.java modified: PatientHandler.java modified: Services.java

--- app/src/main/java/com/well_sync/application/Services.java
+++ app/src/main/java/com/well_sync/application/Services.java
@@ -1,5 +1,6 @@
 package com.well_sync.application;
 
+import com.well_sync.persistence.MoodLogPersistence;
 import com.well_sync.persistence.UserPersistence;
 import com.well_sync.persistence.stubs.UserPersistenceStub;
 
@@ -7,11 +8,22 @@ import com.well_sync.persistence.stubs.UserPersistenceStub;
 public class Services {
     private static UserPersistence userPersistence = null;
 
+    private static final MoodLogPersistence moodLogPersistence = null;
+
     public static synchronized UserPersistence getUserPersistence() {
         if (userPersistence == null) {
             userPersistence = new UserPersistenceStub();
         }
         return userPersistence;
+    }
+
+    public static synchronized MoodLogPersistence getMoodLogPersistence() {
+        if (moodLogPersistence == null) {
+            //Need a mood log persistence stub
+            //moodLogPersistence = new MoodLogPersistenceStub() {
+            //}
+        }
+        return moodLogPersistence;
     }
 
     /**
--- app/src/main/java/com/well_sync/logic/PatientHandler.java
+++ app/src/main/java/com/well_sync/logic/PatientHandler.java
@@ -1,18 +1,30 @@
 package com.well_sync.logic;
 
+import com.well_sync.application.Services;
 import com.well_sync.objects.Patient;
 import com.well_sync.objects.UserCredentials;
+import com.well_sync.persistence.UserPersistence;
 
 public class PatientHandler {
 
+	private final UserPersistence persistUsers;
+
+	public PatientHandler() {
+		persistUsers = Services.getUserPersistence();
+	}
+
+	public PatientHandler(UserPersistence persistence){
+		persistUsers = persistence;
+	}
+
+	//getDetails function will get the patient and give the patient to the UI layer
 	public Patient getDetails(UserCredentials credentials) {
-		// TODO
-		return null;
+		return persistUsers.getPatient(credentials);
 	}
 	
-	public void editDetails(UserCredentials credentials, Patient details) {
-		// TODO
-		return;
+	//editDetails function will get the userInput and set the details into the persistence layer
+	public void editDetails(Patient inputDetails) {
+		persistUsers.setPatient(inputDetails);
 	}
 }
 
