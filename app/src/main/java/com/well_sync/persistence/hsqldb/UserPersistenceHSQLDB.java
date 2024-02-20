8069d2d0f0486aa0faf8a1d6920b8711701b0760
dawoodxv <muhammeddawood414@gmail.com>
2024-02-20 03:54:15 -0600

    User Persistence HSQLDB Boiler Plate

--- app/src/main/java/com/well_sync/persistence/hsqldb/UserPersistenceHSQLDB.java
+++ app/src/main/java/com/well_sync/persistence/hsqldb/UserPersistenceHSQLDB.java
@@ -1,4 +1,43 @@
 package com.well_sync.persistence.hsqldb;
 
-public class UserPersistenceHSQLDB {
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.well_sync.objects.Patient;
+import com.well_sync.objects.UserCredentials;
+import com.well_sync.persistence.IUserPersistence;
+public class UserPersistenceHSQLDB implements IUserPersistence{
+    @Override
+    public UserCredentials getUserCredentials(UserCredentials userCredentials) {
+        return null;
+    }
+
+    @Override
+    public void setUserCredentials(UserCredentials user) {
+
+    }
+
+    @Override
+    public Patient getPatient(UserCredentials userCredentials) {
+        return null;
+    }
+
+    @Override
+    public void setPatient(Patient patient) {
+
+    }
+
+    @Override
+    public Patient getPatient(String email) {
+        return null;
+    }
 }
