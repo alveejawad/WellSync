0076a0932369134f9aa23e7834ba33ad0c80a4b8
dawoodxv <muhammeddawood414@gmail.com>
2024-02-28 15:47:03 -0600

    Created a Medication object

---
+++ app/src/main/java/com/well_sync/objects/Medication.java
@@ -0,0 +1,11 @@
+package com.well_sync.objects;
+
+public class Medication {
+    String name;
+    int quantity; // number of tablets?
+
+    public Medication(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
