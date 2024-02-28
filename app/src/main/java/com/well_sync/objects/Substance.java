bce538de8053da45accd27c28d46159dae3a6c22
dawoodxv <muhammeddawood414@gmail.com>
2024-02-28 16:07:32 -0600

    Create Substance DSO & add in MoodLog

---
+++ app/src/main/java/com/well_sync/objects/Substance.java
@@ -0,0 +1,11 @@
+package com.well_sync.objects;
+
+public class Substance {
+    String name;
+    int quantity; // grams? puffs? lines of coke?
+
+    public Substance(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
