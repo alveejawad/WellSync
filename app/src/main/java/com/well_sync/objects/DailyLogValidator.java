df8affa5a6d10b291cec4461574229523b94f142
Ryan Austria Velicaria <velicar1@myumanitoba.ca>
2024-03-01 12:40:21 -0600

    Update DailyLogValidator with validateSymptomList method

--- app/src/main/java/com/well_sync/objects/DailyLogValidator.java
+++ app/src/main/java/com/well_sync/objects/DailyLogValidator.java
@@ -3,6 +3,7 @@ package com.well_sync.objects;
 import com.well_sync.logic.exceptions.InvalidDailyLogException;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Parent validator for any daily log object.
@@ -16,6 +17,7 @@ public abstract class DailyLogValidator {
         validateDate(log.getDate());
         validateMoodScore(log.getMoodScore());
         validateSleepHours(log.getSleepHours());
+        validateSymptomList(log.getSymptoms());
     }
 
     public static void validateDate(Date date) throws InvalidDailyLogException {
@@ -34,5 +36,15 @@ public abstract class DailyLogValidator {
     public static void validateSleepHours(int sleepHours) throws InvalidDailyLogException {
         if (sleepHours < 0 || sleepHours > 16)
             throw new InvalidDailyLogException("Invalid sleep amount; must be between 0 and 16 hours inclusive.");
+    }
+
+    public static void validateSymptomList(List<Symptom> symptomList) throws InvalidDailyLogException {
+        for (int i = 0; i < symptomList.size(); i++) {
+            if (symptomList.get(i).intensity < 0 || symptomList.get(i).intensity > 5)
+                throw new InvalidDailyLogException("Invalid symptom intensity; must be between 0 and 5 inclusive.");
+
+            if (symptomList.get(i).name == null)
+                throw new InvalidDailyLogException("Invalid symptom name; must have a name");
+        }
     }
 }
