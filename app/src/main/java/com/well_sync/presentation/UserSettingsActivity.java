ff9da1a295bb07a4203777f2f98896ea672469ed
cammartinezx <camila.martinez2002@gmail.com>
2024-03-01 20:50:21 -0600

    Update: Added user details xml and java file

---
+++ app/src/main/java/com/well_sync/presentation/UserSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.well_sync.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.well_sync.R;
+
+public class UserSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings);
+    }
+}
