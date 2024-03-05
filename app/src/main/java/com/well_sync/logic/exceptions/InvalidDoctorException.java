7a3496b4da3240e322081091de55102eb19a5a60
alveejawad <alvee.jawad@gmail.com>
2024-03-05 13:35:42 -0600

    Doctor subclasses

---
+++ app/src/main/java/com/well_sync/logic/exceptions/InvalidDoctorException.java
@@ -0,0 +1,7 @@
+package com.well_sync.logic.exceptions;
+
+public class InvalidDoctorException extends InvalidDataException {
+        public InvalidDoctorException(String error) {
+            super("Invalid doctor details:\n" + error);
+        }
+}
