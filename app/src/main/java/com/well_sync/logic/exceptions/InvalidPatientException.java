2783ce08dbef3adb35a119ce1fd81118ebc9bfec
Gabriel Young <youngg2@myumanitoba.ca>
2024-03-04 22:10:19 -0600

    New InvalidDataException superclass
    All exceptions must now be caught explicitly

--- app/src/main/java/com/well_sync/logic/exceptions/InvalidPatientException.java
+++ app/src/main/java/com/well_sync/logic/exceptions/InvalidPatientException.java
@@ -1,6 +1,6 @@
 package com.well_sync.logic.exceptions;
 
-public class InvalidPatientException extends RuntimeException {
+public class InvalidPatientException extends InvalidDataException {
     public InvalidPatientException(String error) {
         super("Invalid patient details:\n" + error);
     }
