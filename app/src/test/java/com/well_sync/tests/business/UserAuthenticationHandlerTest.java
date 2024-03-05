8d3d609068117259471127bf1bce6f4dbaabf116
ryanv <velicar1@myumanitoba.ca>
2024-03-05 12:20:44 -0600

    Update UserAuthenticationHandlerTest by removing assert methods and adding exceptions Update UserCredentials object with role variable

--- app/src/test/java/com/well_sync/tests/business/UserAuthenticationHandlerTest.java
+++ app/src/test/java/com/well_sync/tests/business/UserAuthenticationHandlerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.well_sync.logic.UserAuthenticationHandler;
+import com.well_sync.logic.exceptions.InvalidCredentialsException;
 import com.well_sync.objects.UserCredentials;
 import com.well_sync.persistence.stubs.UserPersistenceStub;
 
@@ -19,41 +20,48 @@ public class UserAuthenticationHandlerTest {
     }
 
     @Test
-    public void testLogin() {
+    public void testLogin() throws InvalidCredentialsException {
         System.out.println("\nStarting testLogin...");
         UserCredentials creds;
 
         // these credentials are known to be in the database stub
-        creds = new UserCredentials("test123@umanitoba.ca", "test123");
-        assertTrue(userAuthHandler.login(creds));
+        creds = new UserCredentials("test123@umanitoba.ca", "test123", UserCredentials.Role.PATIENT);
+        userAuthHandler.login(creds);
+
+        // these credentials are known to be in the database stub
+        creds = new UserCredentials("test123@umanitoba.ca", "test123", UserCredentials.Role.DOCTOR);
+        userAuthHandler.login(creds);
 
         // this email is valid, but not the password
-        creds = new UserCredentials("test123@umanitoba.ca", "idontknow");
-        assertFalse(userAuthHandler.login(creds));
+        creds = new UserCredentials("test123@umanitoba.ca", "idontknow", UserCredentials.Role.DOCTOR);
+        userAuthHandler.login(creds);
 
         // email does not exist
-        creds = new UserCredentials("unknown@imaginary.com", "foobar");
-        assertFalse(userAuthHandler.login(creds));
+        creds = new UserCredentials("unknown@imaginary.com", "foobar", UserCredentials.Role.DOCTOR);
+        userAuthHandler.login(creds);
 
         System.out.println("Finished testLogin.");
     }
 
     @Test
-    public void testRegister() {
+    public void testRegister() throws InvalidCredentialsException {
         System.out.println("\nStarting testRegister");
         UserCredentials creds;
 
+        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123", UserCredentials.Role.PATIENT);
+        userAuthHandler.register(creds);
+
         // these credentials are well-formed
-        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123");
-        assertTrue(userAuthHandler.register(creds));
+        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         // malformed email address
-        creds = new UserCredentials("cash$$$money@scam.co.uk", "nicepassword123");
-        assertFalse(userAuthHandler.register(creds));
+        creds = new UserCredentials("cash$$$money@scam.co.uk", "nicepassword123", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         // malformed password
-        creds = new UserCredentials("new-user-2@umanitoba.ca", "no spaces allowed");
-        assertFalse(userAuthHandler.register(creds));
+        creds = new UserCredentials("new-user-2@umanitoba.ca", "no spaces allowed", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         System.out.println("Finished testRegister");
     }
