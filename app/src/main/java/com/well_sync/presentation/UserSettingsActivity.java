9da499011315bcae401a6e82c68f7c34e48017c1
cammartinezx <camila.martinez2002@gmail.com>
2024-03-05 15:12:04 -0600

    Update: Include functionality for the buttoms

--- app/src/main/java/com/well_sync/presentation/UserSettingsActivity.java
+++ app/src/main/java/com/well_sync/presentation/UserSettingsActivity.java
@@ -2,15 +2,48 @@ package com.well_sync.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
 import com.well_sync.R;
 
-public class UserSettingsActivity extends AppCompatActivity {
 
+
+public class UserSettingsActivity extends AppCompatActivity {
+    private ImageView closeIcon;
+    private Button editButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        closeIcon=findViewById(R.id.close);
+        editButton=findViewById(R.id.Editbutton);
+        closeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle close button click
+                Intent closeIntent = new Intent(UserSettingsActivity.this, HomePageActivity.class);
+                UserSettingsActivity.this.startActivity(closeIntent);
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle close button click
+                Intent closeIntent = new Intent(UserSettingsActivity.this, UserDetailsActivity.class);
+                UserSettingsActivity.this.startActivity(closeIntent);
+            }
+        });
+
+
+
     }
+
+
+
+
 }
