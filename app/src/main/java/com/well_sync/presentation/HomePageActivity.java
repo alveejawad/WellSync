4dbd4e1e8cd4c62eb9dfb5bf4c8653a0e326d553
cammartinezx <camila.martinez2002@gmail.com>
2024-03-05 15:55:35 -0600

    Update:Included the User Settings in the android manifest

--- app/src/main/java/com/well_sync/presentation/HomePageActivity.java
+++ app/src/main/java/com/well_sync/presentation/HomePageActivity.java
@@ -1,5 +1,4 @@
 package com.well_sync.presentation;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
--- app/src/main/java/com/well_sync/presentation/UserSettingsActivity.java
+++ app/src/main/java/com/well_sync/presentation/UserSettingsActivity.java
@@ -8,19 +8,20 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Button;
 import com.well_sync.R;
-
+import com.well_sync.objects.Patient;
 
 
 public class UserSettingsActivity extends AppCompatActivity {
     private ImageView closeIcon;
     private Button editButton;
+    private Patient patient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
-
         closeIcon=findViewById(R.id.close);
         editButton=findViewById(R.id.Editbutton);
+
         closeIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,16 +35,10 @@ public class UserSettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Handle close button click
-                Intent closeIntent = new Intent(UserSettingsActivity.this, UserDetailsActivity.class);
-                UserSettingsActivity.this.startActivity(closeIntent);
+                Intent editIntent = new Intent(UserSettingsActivity.this, UserDetailsActivity.class);
+                UserSettingsActivity.this.startActivity(editIntent);
             }
         });
-
-
-
     }
-
-
-
 
 }
