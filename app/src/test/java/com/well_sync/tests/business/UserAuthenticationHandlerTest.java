e640053855d07e457ff07b72cdeec934f0a319ec
Ryan Austria Velicaria <velicar1@myumanitoba.ca>
2024-03-06 09:22:04 -0600

    Modify UserAuthenticationHandlerTest - Remove assert statements regarding the userAuthHandler

--- app/src/test/java/com/well_sync/tests/business/UserAuthenticationHandlerTest.java
+++ app/src/test/java/com/well_sync/tests/business/UserAuthenticationHandlerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import com.well_sync.logic.UserAuthenticationHandler;
+import com.well_sync.logic.exceptions.InvalidCredentialsException;
 import com.well_sync.objects.UserCredentials;
 import com.well_sync.persistence.stubs.UserPersistenceStub;
 
@@ -25,15 +26,19 @@ public class UserAuthenticationHandlerTest {
 
         // these credentials are known to be in the database stub
         creds = new UserCredentials("test123@umanitoba.ca", "test123");
-        assertTrue(userAuthHandler.login(creds));
+        userAuthHandler.login(creds);
+
+        // these credentials are known to be in the database stub
+        creds = new UserCredentials("test123@umanitoba.ca", "test123");
+        userAuthHandler.login(creds);
 
         // this email is valid, but not the password
         creds = new UserCredentials("test123@umanitoba.ca", "idontknow");
-        assertFalse(userAuthHandler.login(creds));
+        userAuthHandler.login(creds);
 
         // email does not exist
         creds = new UserCredentials("unknown@imaginary.com", "foobar");
-        assertFalse(userAuthHandler.login(creds));
+        userAuthHandler.login(creds);
 
         System.out.println("Finished testLogin.");
     }
@@ -43,17 +48,20 @@ public class UserAuthenticationHandlerTest {
         System.out.println("\nStarting testRegister");
         UserCredentials creds;
 
+        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123", UserCredentials.Role.PATIENT);
+        userAuthHandler.register(creds);
+
         // these credentials are well-formed
-        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123");
-        assertTrue(userAuthHandler.register(creds));
+        creds = new UserCredentials("new-user@umanitoba.ca", "nicepassword123", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         // malformed email address
-        creds = new UserCredentials("cash$$$money@scam.co.uk", "nicepassword123");
-        assertFalse(userAuthHandler.register(creds));
+        creds = new UserCredentials("cash$$$money@scam.co.uk", "nicepassword123", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         // malformed password
-        creds = new UserCredentials("new-user-2@umanitoba.ca", "no spaces allowed");
-        assertFalse(userAuthHandler.register(creds));
+        creds = new UserCredentials("new-user-2@umanitoba.ca", "no spaces allowed", UserCredentials.Role.DOCTOR);
+        userAuthHandler.register(creds);
 
         System.out.println("Finished testRegister");
     }
